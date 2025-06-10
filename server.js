7566e983b40909be4b97829d3a196a072825023b
Colin de Groot <colindegroot2004@gmail.com>
2025-06-10 12:15:05 +0200

    POST route werkend en one column layout af #9  #4   en opzet voor filter gemaakt #7
    Een werkende post route gemaakt die de data van het formulier ophaalt en meegeeft aan de post. verder extra html zoals formulier en begin filter opvgezet zodat de one column layout issue gesloten kan worden. Enkel nog valideren nu

--- server.js
+++ server.js
@@ -12,26 +12,31 @@ app.engine('liquid', engine.express());
 app.set('view engine', 'liquid');
 app.set('views', './views');
 
-app.get('/', async (request, res) => {
-    try{
-        const experimentsResponse = await fetch('https://open-jii-api-mock.onrender.com/api/v1/experiments?status=published')
-        const experimentsResponseJSON = await experimentsResponse.json();
-        // console.log(experimentsResponseJSON)
-        res.render('index.liquid', { experiments: experimentsResponseJSON })
-    }
-    catch (error){
-        console.error(error)
-        res.status(500).send(error)
-    }
-});
+app.get('/', async (req, res) => {
+  try {
+    const experimentsResponse = await fetch('https://open-jii-api-mock.onrender.com/api/v1/experiments?status=published')
+    const experimentsResponseJSON = await experimentsResponse.json();
+    // console.log(experimentsResponseJSON)
+
+    const experiments = [];
+    experiments.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)); //sort experiments from new to old 
+
+
+    res.render('index.liquid', { experiments: experimentsResponseJSON })
+  }
+  catch (error) {
+    console.error(error)
+    res.status(500).send(error)
+  }
+}); 
 
 
 app.get('/experiment/:id', async (req, res) => {
   try {
- 
+    // make a page for the selected project on the homepage
     const experimentId = req.params.id;
-  
-    const homeResponse = await fetch(`https://open-jii-api-mock.onrender.com/api/v1/experiments/${experimentId}`);
+
+    const homeResponse = await fetch(`https://open-jii-api-mock.onrender.com/api/v1/experiments/${experimentId}`); //get the data of selected project with the filter
     if (!homeResponse.ok) {
       return res.status(homeResponse.status).send("Experiment not found");
     }
@@ -44,6 +49,38 @@ app.get('/experiment/:id', async (req, res) => {
     res.status(500).send("something went wrong");
   }
 });
+
+app.post('/create-experiment', async (req, res) => {
+  try {
+    //Get data from the input fields and pass them to the POST 
+    const { name, description } = req.body;
+
+    const response = await fetch('https://open-jii-api-mock.onrender.com/api/v1/experiments', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name,
+        description,
+        status: "published", // Setting everything on public for now since this is still in testing fase and all post should be directly visible for testing pruposes
+        visibility: "public", // same thing for the others
+        embargoIntervalDays: 0
+      })
+    });
+
+    if (!response.ok) {
+      throw new Error(`API returned status ${response.status}`);
+    }
+
+    // Redirect back to home after posting
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Er ging iets mis bij het maken van een experiment.');
+  }
+});
+
+
+
 
 
 // Stel de poort in
