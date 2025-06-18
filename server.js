00b8fdc914acb659198c751e25006894b484d7d9
Colin de Groot <colindegroot2004@gmail.com>
2025-06-18 09:45:44 +0200

    code simplify, skip link en states toegevoegd aan de post #9  #10

--- server.js
+++ server.js
@@ -33,62 +33,58 @@ app.get('/', async (req, res) => {
 
 
 app.get('/experiment/:id', async (req, res) => {
-    // make a page for the selected project on the homepage
-    const experimentId = req.params.id;
+  // make a page for the selected project on the homepage
+  const experimentId = req.params.id;
 
-    const homeResponse = await fetch(`https://open-jii-api-mock.onrender.com/api/v1/experiments/${experimentId}`); //get the data of selected project with the filter
+  const homeResponse = await fetch(`https://open-jii-api-mock.onrender.com/api/v1/experiments/${experimentId}`); //get the data of selected project with the filter
 
-    const experimentData = await homeResponse.json();
+  const experimentData = await homeResponse.json();
 
-    let experimentColumns
+  let experimentColumns
 
-    if (experimentData.data){ 
-       experimentColumns = experimentData.data.columns
-    }
-    // console.log(experimentColumns) 
+  if (experimentData.data) {
+    experimentColumns = experimentData.data.columns
+  }
 
-    res.render('experiment', { experiment: experimentData, column: experimentColumns});
+  // console.log(experimentColumns) 
+
+  res.render('experiment', { experiment: experimentData, column: experimentColumns });
 });
 
 app.post('/create-experiment', async (req, res) => {
-  try {
-    //Get data from the input fields and pass them to the POST 
-    const { name, description } = req.body;
+  //Get data from the input fields and pass them to the POST 
+  const { name, description } = req.body;
 
-    const response = await fetch('https://open-jii-api-mock.onrender.com/api/v1/experiments', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        id: uuidv4(), //render requires a uuid to be able to search on id. When posting the posted experiment doesnt get an 
-        name,
-        description,
-        status: "published", // Setting everything on public for now since this is still in testing fase and all post should be directly visible for testing pruposes
-        visibility: "public", // same thing for the others
-        embargoIntervalDays: 0,
-        createdAt: (new Date()).toISOString()  //doesn't get pushed otherwise
-      })
-    });
+  const response = await fetch('https://open-jii-api-mock.onrender.com/api/v1/experiments', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      id: uuidv4(), //render requires a uuid to be able to search on id. When posting the posted experiment doesnt get an 
+      name,
+      description,
+      status: "published", // Setting everything on public for now since this is still in testing fase and all post should be directly visible for testing pruposes
+      visibility: "public", // same thing for the others
+      embargoIntervalDays: 0,
+      createdAt: (new Date()).toISOString(),  //doesn't get pushed otherwise
+      updatedAt: (new Date()).toISOString()
+    })
+  });
 
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Something went wrong deleting the experiment.', error);
-  }
+  res.redirect('/experiments?newlyAdded=true');
+
+
 });
 
 app.post('/experiment/:id/delete', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const response = await fetch(
-      `https://open-jii-api-mock.onrender.com/api/v1/experiments/${id}`,
-      { method: 'DELETE' }
-    );
 
-    return res.redirect('/');
+  const { id } = req.params;
+  const response = await fetch(
+    `https://open-jii-api-mock.onrender.com/api/v1/experiments/${id}`,
+    { method: 'DELETE' }
+  );
 
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error deleting experiment!');
-  }
+  return res.redirect('/');
+
 });
 
 
