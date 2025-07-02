6655f92c7ebcbe1f8d5692eef4846448702eb974
Colin de Groot <colindegroot2004@gmail.com>
2025-07-02 13:11:44 +0200

    confirm delete  #18  en js mini cleanup
    met een window venster de user een bevestiging geven voor het verwijderen van een project en verscvhuiven van scripts voor snellere laadtijden

--- public/js/script.js
+++ public/js/script.js
@@ -9,3 +9,17 @@ document.querySelectorAll('.experiment-link').forEach(link => {
     });
   });
 });
+
+// delete project conformation
+document.addEventListener('click', function (event) {
+  const confirmBtn = event.target.closest('.delete-button'); // start event on selected element in this case the selected button
+  if (!confirmBtn) return; //if there is no button the conformation will be cancelled
+
+  const conformation = confirm("Are you sure you want to delete this experiment?");
+  if (!conformation) {
+    event.preventDefault(); //prevent the reload in which the experiment will be deleted
+    event.stopImmediatePropagation(); // prevent other listeners of the same event from being called. This cancells the delete and other events connected to the button
+  }
+
+}, true);
+
